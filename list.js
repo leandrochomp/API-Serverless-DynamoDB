8cee1c3cdcbe572d0fa74b33c911ae04d3baa415
leandrochomp <chomp.vercetti@gmail.com>
2021-05-23 18:53:39 +1000

    Adding promise/async into the list

--- list.js
+++ list.js
@@ -2,17 +2,28 @@
 
 const AWS = require("aws-sdk");
 
-const dynamoDB = new AWS.dynamoDB.DocumentClient();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const params = {
     TableName: process.env.DYNAMODB_TABLE
 }
 
+//Function to list items from DB
 module.exports.list = async (event, context, callback) => {
-    dynamoDB.scan(params, (error, result) => {
-        const response = {
-            statusCode:200,
-            body: JSON.stringify(result.Items),
-        };
-        callback(null, response);
-    });
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE
+    }
+    try {
+        await dynamoDb.scan(params, (error, result) => {
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(result.Items),
+            };
+
+            callback(null, response);
+
+        }).promise();
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
 };
