8a94e946c08e9c4fc2a3b84ba3962178f688fc94
leandrochomp <chomp.vercetti@gmail.com>
2021-05-23 18:52:58 +1000

    Adding async, removing uuid, adding crypto

--- create.js
+++ create.js
@@ -1,25 +1,37 @@
+
 'use strict';
 
-const uuid = require('uuid');
 const AWS = require("aws-sdk");
+const crypto = require("crypto");
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.create = async (event, context, callback) => {
-    const data = JSON.parse(event.body);
-    const params = {
-        TableName: process.env.DYNAMODB_TABLE,
-        Item: {
-            id: uuid.v1(),
-            text: data.text
-        }
-    };
+// Generate unique id with no external dependencies
+const generateUUID = () => crypto.randomBytes(16).toString("hex");
 
-    dynamoDb.put(params, (error) => {
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify(params.Item),
+//Function to create an Item to DB
+module.exports.create = async (event, context, callback) => {
+    try {
+        const data = JSON.parse(event.body);
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE,
+            Item: {
+                id: generateUUID(),
+                name: data.text
+            }
         };
-        callback(null, response);
-    });
+
+        await dynamoDb.put(params, (error) => {
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(params.Item),
+            };
+
+            callback(null, response);
+
+        }).promise();
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
 };
